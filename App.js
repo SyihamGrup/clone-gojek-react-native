f4a7ab2a0ec811836796802b4db14fba06a27ae1
Muhamad Danil Rafiqi <danil.rafiqi@gmail.com>
2019-04-26 09:52:04 +0700

    Cloning Gojek App - Search Bar and Promo Section #10 React Native Tutorial [Indonesia]

--- App.js
+++ App.js
@@ -1,11 +1,48 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, TextInput } from 'react-native';
 
 export default class App extends Component {
   render() {
     return (
       <View style={{ flex: 1 }}>
-        <View style={{ backgroundColor: 'pink', flex: 1 }} />
+        <View style={{ backgroundColor: 'white', flex: 1 }}>
+          <View
+            style={{
+              marginHorizontal: 17,
+              flexDirection: 'row',
+              paddingTop: 15
+            }}>
+            <View style={{ position: 'relative', flex: 1 }}>
+              <TextInput
+                placeholder="What do you want to eat?"
+                style={{
+                  borderWidth: 1,
+                  borderRadius: 25,
+                  borderColor: '#E8E8E8',
+                  height: 40,
+                  fontSize: 13,
+                  paddingLeft: 45,
+                  paddingRight: 20,
+                  backgroundColor: 'white',
+                  marginRight: 18
+                }}
+              />
+              <Image
+                source={require('./icons/help.png')}
+                style={{ position: 'absolute', top: 5, left: 6 }}
+              />
+            </View>
+            <View
+              style={{
+                width: 35,
+                alignItems: 'center',
+                justifyContent: 'center'
+              }}>
+              <Image source={require('./icons/help.png')} />
+            </View>
+          </View>
+        </View>
+
         <View style={{ height: 54, flexDirection: 'row' }}>
           <View
             style={{
